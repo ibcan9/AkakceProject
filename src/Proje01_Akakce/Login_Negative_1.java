727104f00efb5f8bcec584f51a2330a92eeabf8c
ahyat <gokdemir.aysenur@gmail.com>
2025-03-21 20:34:41 +0300

    US_104 Giriş Yap-Pozitif/ US_104.1 Giriş Yap-Negatif yapıldı. Ayşenur Can

---
+++ src/Proje01_Akakce/Login_Negative_1.java
@@ -0,0 +1,39 @@
+package Proje01_Akakce;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Login_Negative_1 extends BaseDriver {
+    @Test
+    public void Login(){
+
+
+        driver.get("https://www.akakce.com/");
+
+        WebElement girisYap= driver.findElement(By.cssSelector("div[class='g-3_v8 hm_hp_v8']>div > a[href='/akakcem/giris/']"));
+        girisYap.click();
+        MyFunc.Wait(2);
+
+        WebElement eposta= driver.findElement(By.cssSelector("form[id='FrmLi']> span> input"));
+        eposta.sendKeys("ennurgl25@gmail.com");
+        MyFunc.Wait(2);
+
+        WebElement sifre= driver.findElement(By.cssSelector("form[id='FrmLi']> span> input[type='password']"));
+        sifre.sendKeys("ahahahha");
+        MyFunc.Wait(2);
+
+        WebElement beniHatirla= driver.findElement(By.cssSelector("span[class='checkbox-wrapper']+label"));
+        beniHatirla.click();
+        MyFunc.Wait(2);
+
+        WebElement girisYap2= driver.findElement(By.cssSelector("a[class='fp']+input"));
+        girisYap2.click();
+        MyFunc.Wait(2);
+
+
+        WaitAndClose();
+    }
+}
